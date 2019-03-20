20540ab27a460348e6aa92e2838204ef9f6b4f5a
pavrehal <48033022+pavrehal@users.noreply.github.com>
2019-03-20 14:40:17 +0000

    Update my-script.sh

--- scripts/my-script.sh
+++ scripts/my-script.sh
@@ -5,5 +5,5 @@ mkdir -p analysis docs data
 # add a README.md to each directory
 for my_directory in scripts analysis docs data;do
   touch ${my_directory}/README.md
-  echo “# ${my_directory}” >> ${my_directory}/README.md
+  echo "# ${my_directory}" >> ${my_directory}/README.md
 done
